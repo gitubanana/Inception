7446ddee0b46f893fd6f1f0eeccb4ccd6a2f08a0
Ubuntu <ubuntu@ip-172-31-40-123.ap-northeast-2.compute.internal>
2023-08-26 12:48:49 +0000

    서브젝트에 맞게 파일 경로 설정

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -7,10 +7,8 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER \
 					--admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL --skip-email
   	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PW
-	wp theme activate twentytwentytwo
 else
 	echo wordpress 이미 완성됨
 fi
 
-# wp server --host=0.0.0.0 --port=9000
-/usr/sbin/php-fpm81 --nodaemonize
+php-fpm81 --nodaemonize
