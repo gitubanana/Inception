d06a6de7ef5edcda7015e570186092ad8d952d14
Ubuntu <ubuntu@ip-172-31-40-123.ap-northeast-2.compute.internal>
2023-09-06 07:25:15 +0000

    adminer 서비스 추가

---
+++ srcs/requirements/bonus/adminer/tools/adminer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -f $ADMINER_FILE ]; then
+    echo Adminer 이미 완성됨
+else
+    curl -L -o $ADMINER_FILE \
+        https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php
+fi
+
+php -S adminer:8080
